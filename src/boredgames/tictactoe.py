bb79e6f7b51a769ad2e6f8c9e7628d723a772ddc
Troy Gilmore <troygilmore89@gmail.com>
2022-11-08 15:10:37 -0600

    Occupying cells on board
    Cycle through players
    Create moves
    Create board with cells.
    Cells have states empty and occupied.

--- src/boredgames/tictactoe.py
+++ src/boredgames/tictactoe.py
@@ -1,13 +1,118 @@
 from __future__ import annotations
 from typing import Protocol
+from dataclasses import dataclass
+from itertools import cycle
+from abc import ABC, abstractmethod
 
 class Presenter(Protocol):
     def winner(self, player:str) -> None:
         ...
 
+    def move_made(self, move:Move) -> None:
+        ...
+
 class TicTacToe:
-    def __init__(self, presenter):
+    def __init__(self, presenter:Presenter):
         self._presenter = presenter
+        self._board = Board()
+
+        players = (Player('X'), Player('O'))
+        self.players = cycle(players)
+        self._next_player()
+
+    @property
+    def current_player(self) -> Player:
+        return self._current_player
+
+    @current_player.setter
+    def current_player(self, player:Player) -> None:
+        self._current_player = player
+
+    @property
+    def board(self) -> Board:
+        return self._board
 
     def move(self, row:int, col:int):
-        ...
+        move = Move(self.current_player)
+        self.board.at(row, col).occupy(move)
+        self._presenter.move_made(move)
+        self._next_player()
+
+    def _next_player(self):
+        self.current_player = next(self.players)
+        
+
+@dataclass
+class Move:
+    player:Player
+
+@dataclass
+class Player:
+    marker:str
+
+class Cell:
+    class CellState(ABC):
+        @abstractmethod
+        def is_occupied(self) -> bool:
+            pass
+        @abstractmethod
+        def occupy(self, context:Cell, move:Move)->None:
+            pass
+    
+    class EmptyState(CellState):
+        def is_occupied(self) -> bool:
+            return False
+        def occupy(self, context: Cell, move: Move) -> None:
+            context._player = move.player
+            context._current_state = Cell.OCCUPIED
+        
+    class OccupiedState(CellState):
+        def is_occupied(self) -> bool:
+            return True
+        def occupy(self, context: Cell, move: Move) -> None:
+            pass
+
+    EMPTY = EmptyState()
+    OCCUPIED = OccupiedState()
+
+    def __init__(self, board:Board, row:int, col:int):
+        self.board:Board = board
+        self._row:int = row
+        self._col:int = col
+        self._current_state:Cell.CellState = self.EMPTY
+        self._player = None
+
+    @property
+    def is_occupied(self) -> bool:
+        return self._current_state.is_occupied()
+
+    @property
+    def row(self) -> int:
+        return self._row
+
+    @property
+    def col(self) -> int:
+        return self._col
+
+    def occupy(self, move:Move) -> None:
+        self._current_state.occupy(self, move)
+
+class Board:
+    def __init__(self) -> None:
+        self._cells = list()
+        for i in range(self.rows):
+            row = list()
+            for j in range(self.cols):
+                row.append(Cell(self, i, j))
+            self._cells.append(row)
+
+    @property
+    def rows(self) -> int:
+        return 3
+
+    @property
+    def cols(self) -> int:
+        return 3
+
+    def at(self, row:int, col:int) -> Cell:
+        return self._cells[row][col]
--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -1,29 +1,97 @@
 import pytest
-from boredgames import tictactoe
+from dataclasses import dataclass
+from boredgames.tictactoe import TicTacToe, Presenter, Move, Cell, Board, Player
 
-class SpyPresenter(tictactoe.Presenter):
+class SpyPresenter(Presenter):
+    def winner(self, player: str) -> None:
+        self.the_winner = player
 
-    def __init__(self, winner:str) -> None:
-        self.winner = winner
+    def move_made(self, move:Move) -> None:
+        self.the_move_made = move
 
-    def winner(self, player:str):
-        assert self.winner == player
+@dataclass
+class GameTuple():
+    game:TicTacToe
+    presenter:SpyPresenter
 
 @pytest.fixture
-def game() -> tictactoe:
-    return tictactoe(presenter=SpyPresenter(winner="X"))
+def game_tuple() -> tuple:
+    presenter = SpyPresenter()
+    return GameTuple(TicTacToe(presenter=presenter), presenter)
 
 
 class TestAcceptance():
 
-    def test_tictactoe_game_ends_in_win(self, game:tictactoe.TicTacToe):
+    def test_tictactoe_game_ends_in_win(self, game_tuple:GameTuple):
+        game = game_tuple.game
+        presenter = game_tuple.presenter
+        winner = game.current_player
         game.move(0,0)
         game.move(2,0)
         game.move(0,1)
         game.move(2,1)
         game.move(0,2)
+        assert presenter.the_winner == winner
     
-
 class TestGame:
-    def test_move_plays_for_current_player(self):
-        ...
+    
+    def test_move_plays_for_current_player(self, game_tuple:GameTuple):
+        game = game_tuple.game
+        expected = game.current_player
+        game.move(0,0)
+        actual = game.current_player
+        assert actual != expected
+
+    def test_successful_move_notives_presenter_of_move(self, game_tuple:GameTuple):
+        presenter = game_tuple.presenter
+        game = game_tuple.game
+        player = game.current_player
+        game.move(0, 0)
+        assert presenter.the_move_made.player == player
+
+    def test_move_updates_board(self, game_tuple:GameTuple):
+        game = game_tuple.game
+        game.move(0,0)
+        cell:Cell = game.board.at(0, 0)
+        assert cell.is_occupied
+
+class TestBoard:
+
+    @pytest.fixture
+    def board(self)->Board:
+        return Board()
+
+    def test_board_has_three_rows(self, board:Board):
+        assert 3 == board.rows
+
+    def test_board_has_three_cols(self, board:Board):
+        assert 3 == board.cols
+
+    def test_created_board_has_empty_cells(self, board:Board):
+        for i, row in enumerate(board._cells):
+            for j, cell in enumerate(row):
+                assert cell.is_occupied == False
+
+class SpyBoard:
+    pass
+
+class TestCell:
+    @pytest.fixture
+    def cell(self) -> Cell:
+        return Cell(SpyBoard(), row=0, col=0)
+
+    def test_cell_returns_row(self, cell:Cell):
+        assert 0 == cell.row
+
+    def test_cell_returns_col(self, cell:Cell):
+        assert 0 == cell.col
+
+    def test_cell_is_empty_at_creation(self, cell:Cell):
+        assert cell.is_occupied == False
+
+    def test_cell_is_occupied_after_move(self, cell:Cell):
+        move = Move(Player("X"))
+        cell.occupy(move)
+        assert cell.is_occupied == True
+
+
